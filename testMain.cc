d114d2eca52d1cacdfb7d1bee55ff4b94bf31bad
Tung Nguyen <tungnguyen@Tungs-MacBook-Pro.local>
2014-09-25 19:00:23 -0700

    - Added glfw library with a simple example. - to build first install cmake (sudo apt-get install cmake) then call build.sh

---
+++ testMain.cc
@@ -0,0 +1,64 @@
+
+#include "GLFW/glfw3.h"
+#include <stdlib.h>
+#include <stdio.h>
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+int main(void)
+{
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+     float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+        glViewport(0, 0, width, height);
+        glColor3f(1.f, 1.f, 1.f);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        //glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+        glTranslatef(ratio/2.0,0.0,0.0);
+        glScalef(0.1,0.1,0.1);
+       
+        glBegin(GL_POLYGON);
+
+        glVertex3f(-1.0f, 0.0f, 0.f);
+        
+        glVertex3f(-1.0f, 1.0f, 0.f);
+
+        glVertex3f(0.0f, 1.0f, 0.f);
+
+        glVertex3f(0.0f, 0.0f, 0.f);
+        
+        glEnd();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
